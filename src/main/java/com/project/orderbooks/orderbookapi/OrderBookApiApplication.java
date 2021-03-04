4a51565e490d51d730ac8d540ca76f46fb715d42
eren doganay <erendoganay60@hotmail.com>
2021-03-04 20:27:23 +0300

    initial commit

---
+++ src/main/java/com/project/orderbooks/orderbookapi/OrderBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.orderbooks.orderbookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderBookApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderBookApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/orderbooks/orderbookapi/OrderBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.orderbooks.orderbookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderBookApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
